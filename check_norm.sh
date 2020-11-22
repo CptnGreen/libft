674d4f0c2c2008d181fad8660ec31ae798f7ed57
CptnGreen <a.krasnoshchekov1992@gmail.com>
2020-11-23 00:41:29 +0300

    Norm script? (2)

---
+++ check_norm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare log="norminette.txt"
+declare norminette="~/.norminette/norminette.rb"
+
+main()
+{
+	rm "${log}"
+	"${norminette}" src includes 2>/dev/null > "${log}"
+	cat "${log}"
+	if cat "${log}" | grep "Error"; then
+		exit 1
+	else
+		exit 0
+	fi
+}
+
+main
