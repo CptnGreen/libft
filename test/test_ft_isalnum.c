e8cc8ce778ea4c9ed496a05b1a84dea6cfb0bc21
CptnGreen <a.krasnoshchekov1992@gmail.com>
2020-11-30 01:50:14 +0300

    Update isalnum tests

--- test/test_ft_isalnum.c
+++ test/test_ft_isalnum.c
@@ -23,49 +23,58 @@ void tearDown(void)
 
 void test_zero_char(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum('0'), ft_isalnum('0'));
+    // TEST_ASSERT_EQUAL_INT(isalnum('0'), ft_isalnum('0'));
+    TEST_ASSERT_GREATER_THAN(0, ft_isalnum('0'));
 }
 
 void test_three_char(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum('3'), ft_isalnum('3'));
+    // TEST_ASSERT_EQUAL_INT(isalnum('3'), ft_isalnum('3'));
+    TEST_ASSERT_GREATER_THAN(0, ft_isalnum('3'));
 }
 
 void test_a(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum('a'), ft_isalnum('a'));
+    // TEST_ASSERT_EQUAL_INT(isalnum('a'), ft_isalnum('a'));
+    TEST_ASSERT_GREATER_THAN(0, ft_isalnum('a'));
 }
 
 void test_big_a(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum('A'), ft_isalnum('A'));
+    // TEST_ASSERT_EQUAL_INT(isalnum('A'), ft_isalnum('A'));
+    TEST_ASSERT_GREATER_THAN(0, ft_isalnum('A'));
 }
 
 /*
 ** Is not alnum:
 */
 
-void test_three_abs(void)
+void test_three_asci(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum(3), ft_isalnum(3));
+    // TEST_ASSERT_EQUAL_INT(isalnum(3), ft_isalnum(3));
+    TEST_ASSERT_EQUAL_INT(0, ft_isalnum(3));
 }
 
 void test_dot(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum('.'), ft_isalnum('.'));
+    // TEST_ASSERT_EQUAL_INT(isalnum('.'), ft_isalnum('.'));
+    TEST_ASSERT_EQUAL_INT(0, ft_isalnum('.'));
 }
 
 void test_max(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum(UCHAR_MAX), ft_isalnum(UCHAR_MAX));
+    // TEST_ASSERT_EQUAL_INT(isalnum(UCHAR_MAX), ft_isalnum(UCHAR_MAX));
+    TEST_ASSERT_EQUAL_INT(0, ft_isalnum(UCHAR_MAX));
 }
 
 void test_almost_max(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum(UCHAR_MAX - 1), ft_isalnum(UCHAR_MAX - 1));
+    // TEST_ASSERT_EQUAL_INT(isalnum(UCHAR_MAX - 1), ft_isalnum(UCHAR_MAX - 1));
+    TEST_ASSERT_EQUAL_INT(0, ft_isalnum(UCHAR_MAX - 1));
 }
 
 void test_min(void)
 {
-    TEST_ASSERT_EQUAL_INT(isalnum(0), ft_isalnum(0));
+    // TEST_ASSERT_EQUAL_INT(isalnum(0), ft_isalnum(0));
+    TEST_ASSERT_EQUAL_INT(0, ft_isalnum(0));
 }
